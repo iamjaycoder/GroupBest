e823843e34b213cb769d2d4dff1d440d0b4b1675
IbukunItech <ibukunoluwafamuyiwa@gmail.com>
2021-10-21 13:49:18 -0700

    Ibk component, nav component

--- src/groupbest/trello/header.jsx
+++ src/groupbest/trello/header.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import styled from "styled-components";
+import img from "../asset/logo.svg";
 
 const Header = () => {
   return (
     <Container>
       <Wrapper>
-        <Logo>Trello</Logo>
+        <Logo src={img}/>
         <Nav>
           <Bar> Log in </Bar>
           <Button>Sign up</Button>
@@ -19,26 +20,26 @@ export default Header;
 
 const Container = styled.div`
   width: 100%;
-  height: 100px;
+  height: 70px;
   background: #eae6ff;
 `;
 const Wrapper = styled.div`
   width: 100%;
-  height: 100px;
+  height: 100%;
   display: flex;
   justify-content: space-between;
   align-items: center;
 `;
-const Logo = styled.div`
+const Logo = styled.img`
   width: 200px;
   height: 50px;
   object-fit: contain;
   margin-left: 20px;
   // background-color : red;
-  font-size: 40px;
-  color: #5a6480;
-  letter-spacing: 5;
-  font-weight: bold;
+  // font-size: 40px;
+  // color: #5a6480;
+  // letter-spacing: 5;
+  // font-weight: bold;
 
   :hover {
     cursor: pointer;
@@ -50,29 +51,32 @@ const Nav = styled.div`
   align-items: center;
 `;
 const Bar = styled.div`
-  color: #3d9de8;
-  // font-weight: bold;
-  margin: 0 10px;
-  font-size: 20px;
+  color: #0052cc;
+  // font-weight: bold
+  margin: 0 20px;
+  font-size: 15px;
+  font-family: sans-serif;
+  text-decoration: underline;
+
 
   :hover {
-    text-decoration: underline;
     cursor: pointer;
   }
 `;
 const Button = styled.div`
-  background-color: blue;
-  width: 88px;
-  height: 38px;
-  border-radius: 4px;
-  color: #f7ffff;
-  font-size: 20px;
+  background-color:#2F54FF;
+  width: 85px;
+  height: 35px;
+  border-radius: 5px;
+  color: #FFFFFA;
+  font-size: 15px;
+  font-family: sans-serif;
   display: flex;
   align-items: center;
   justify-content: center;
 
   :hover {
-    background-color: #0051cc;
+    background-color: #0052cc;
     cursor: pointer;
   }
 `;
